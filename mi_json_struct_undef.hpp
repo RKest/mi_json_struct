0690ef151160ad6bbdcd38c9ac4f606ef9f89a4c
RKest <max.ind@o2.pl>
2024-02-27 17:03:48 +0100

    Added undef header

---
+++ mi_json_struct_undef.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#undef p_MI_FE_0
+#undef p_MI_FE_1
+#undef p_MI_FE_2
+#undef p_MI_FE_3
+#undef p_MI_FE_4
+#undef p_MI_FE_5
+#undef p_MI_FE_6
+#undef p_MI_FE_7
+#undef p_MI_FE_8
+#undef p_MI_FE_9
+#undef p_MI_FE_10
+#undef p_MI_FE_11
+#undef p_MI_FE_12
+#undef p_MI_FE_13
+#undef p_MI_FE_14
+#undef p_MI_FE_15
+#undef p_MI_FE_16
+#undef p_MI_FE_17
+#undef p_MI_FE_18
+#undef p_MI_FE_19
+#undef p_MI_FE_20
+#undef p_MI_FE_21
+#undef p_MI_FE_22
+#undef p_MI_FE_23
+#undef p_MI_FE_24
+#undef p_MI_FE_25
+#undef p_MI_FE_26
+#undef p_MI_FE_27
+#undef p_MI_FE_28
+#undef p_MI_FE_29
+#undef p_MI_FE_30
+#undef p_MI_FE_31
+#undef p_MI_FE_32
+#undef p_MI_FE_33
+#undef p_MI_FE_34
+#undef p_MI_FE_35
+#undef p_MI_FE_36
+#undef p_MI_FE_37
+#undef p_MI_FE_38
+#undef p_MI_FE_39
+#undef p_MI_FE_40
+#undef p_MI_FE_41
+#undef p_MI_FE_42
+#undef p_MI_FE_43
+#undef p_MI_FE_44
+#undef p_MI_FE_45
+#undef p_MI_FE_46
+#undef p_MI_FE_47
+#undef p_MI_FE_48
+#undef p_MI_FE_49
+#undef p_MI_FE_50
+#undef p_MI_FE_51
+#undef p_MI_FE_52
+#undef p_MI_FE_53
+#undef p_MI_FE_54
+#undef p_MI_FE_55
+#undef p_MI_FE_56
+#undef p_MI_FE_57
+#undef p_MI_FE_58
+#undef p_MI_FE_59
+#undef p_MI_FE_60
+#undef p_MI_FE_61
+#undef p_MI_FE_62
+#undef p_MI_FE_63
+#undef p_MI_FE_64
+
+#undef p_MI_EXPAND
+
+#undef p_MI_FOR_EACH_IMPL
+#undef p_MI_FOR_EACH
+
+#undef p_MI_FIRST_IMPL
+#undef p_MI_FIRST
+#undef p_MI_SECOND_IMPL
+#undef p_MI_SECOND
+#undef p_MI_STRINGIZE_IMPL
+#undef p_MI_STRINGIZE
+
+#undef p_MI_FROM_JSON
+#undef p_MI_TO_JSON
+
+#undef p_MI_JSON_MEMBER
+#undef p_MI_JSON_STRUCT
+
+#undef MI_STRUCT_WITH_FROM_JSON
+
+#undef MI_STRUCT_WITH_TO_JSON
+
+#undef MI_STRUCT_WITH_FROM_AND_TO_JSON
